516743ec18451fd4ec751c39ba07a3f0d7142641
shisan <shisan@tsign.cn>
2024-10-31 01:29:30 +0800

    feat: ll

--- src/app/ui/episodeDetail/transcript.tsx
+++ src/app/ui/episodeDetail/transcript.tsx
@@ -47,7 +47,7 @@ export function Transcript({ data }: { data: any }) {
               )}
               <div className={`flex flex-col justify-between`}>
                 <div
-                  className={`text-min rounded-[8px] pl-[16px] pr-[5px] inline-block cursor-pointer`}
+                  className={`text-min rounded-[8px] pl-[16px] pr-[5px] inline-block cursor-pointer self-start`}
                   onClick={(e: any) => playCurrTime(item.start, e)}
                   style={{ background: speaker.bg }}
                 >
