63b49fe13a5744f09cf3a2c96a9122af8efcb456
shisan <shisan@tsign.cn>
2024-11-25 17:07:43 +0800

    feat: 暂存

--- src/app/followed-podcasts-all/(overview)/page.tsx
+++ src/app/followed-podcasts-all/(overview)/page.tsx
@@ -2,10 +2,12 @@ import Pagination from '@/app/ui/pagination'
 import { getPodShow } from '@/app/lib/service'
 import { getMetaData } from '@/app/lib/utils'
 import { Metadata } from 'next'
+import Link from 'next/link'
 const y = new Date().getFullYear()
+import SearchPodcastCard from '@/app/ui/search/search-podcast-card'
 export const metadata: Metadata = getMetaData({
-  title: `The Latest Episodes of  ${y - 1}-${y} | PodExtra.AI`,
-  description: `PodExtra keeps a close eye on Latest Episodes, bringing you updates on popular Episodes as soon as they're available, ensuring that you can transcribe and summarize the content right away.`,
+  title: `Followed Podcasts | PodExtra.AI`,
+  description: `Check out the podcasts you've followed. PodExtra's features like transcripts, etc., enable quick access and better experience. All your followed podcasts are ready for you to dive into again.`,
   keywords: '',
 })
 export default async function Page({
@@ -16,17 +18,40 @@ export default async function Page({
     page?: string
   }
 }) {
-  const { pageSize = 10, page: pageNum = 1 } = searchParams || {}
+  const { pageSize = 50, page: pageNum = 1 } = searchParams || {}
   const {
     data: { resultList = [], total = 0 },
   } = await getPodShow({ pageSize, pageNum, sortBy: 'PUB_DATE' })
   const totalPages = Math.ceil(+total / +pageSize)
   return (
-    <main className="flex flex-col">
-      <div className={`sticky top-[40px] bg-white dark:bg-black pb-[22px]`}>
-        <Pagination totalPages={totalPages} total={total} title="podcasts" />
+    <main className="flex flex-col mt-[3px]">
+      <div className={`flex items-center mb-[20px]`}>
+        <Link
+          href={'/followed-podcasts-updated'}
+          className={`text-sm font-semibold px-[10px] text-fontGry-600 py-[8px] bg-bgGray mr-[13px] rounded-[10px]`}
+        >
+          Updated episodes
+        </Link>
+        <Link href={''} className={`text-sm font-semibold text-white px-[10px] py-[8px] bg-play rounded-[10px]`}>
+          All podcasts
+        </Link>
       </div>
-      <div className={`flex flex-wrap border border-gray-1000 rounded-10px p-[14px] dark:border-fontGry-600`}></div>
+      <div className={`sticky top-[57px] bg-white dark:bg-black pb-[22px] z-[99]`}>
+        <Pagination totalPages={totalPages} total={total} title="episodes" />
+      </div>
+      <div className={`flex flex-wrap border border-gray-1000 rounded-10px p-[14px] dark:border-fontGry-600 overflow-hidden`}>
+        {resultList.map((item: any, ind: number) => {
+          const { coverUrl, categoryList, showId, itunesAuthor, showTitle, showDescription, gmtLastUpdate, showUrl } = item
+          const noMb = ind >= resultList.length - 1
+          return (
+            <SearchPodcastCard
+              item={{ coverUrl, categoryList, showId, itunesAuthor, showTitle, showDescription, gmtLastUpdate, showUrl }}
+              noMb={noMb}
+              key={showId}
+            />
+          )
+        })}
+      </div>
     </main>
   )
 }
--- src/app/followed-podcasts-updated/(overview)/page.tsx
+++ src/app/followed-podcasts-updated/(overview)/page.tsx
@@ -1,9 +1,10 @@
 import Pagination from '@/app/ui/pagination'
-import { getPodShow } from '@/app/lib/service'
+import { getPodEpisode } from '@/app/lib/service'
 import { getMetaData } from '@/app/lib/utils'
 import { Metadata } from 'next'
 import Link from 'next/link'
 const y = new Date().getFullYear()
+import SearchEpisodesCard from '@/app/ui/search/search-episodes-card'
 export const metadata: Metadata = getMetaData({
   title: `Followed Podcasts | PodExtra.AI`,
   description: `Check out the podcasts you've followed. PodExtra's features like transcripts, etc., enable quick access and better experience. All your followed podcasts are ready for you to dive into again.`,
@@ -17,10 +18,10 @@ export default async function Page({
     page?: string
   }
 }) {
-  const { pageSize = 10, page: pageNum = 1 } = searchParams || {}
+  const { pageSize = 50, page: pageNum = 1 } = searchParams || {}
   const {
     data: { resultList = [], total = 0 },
-  } = await getPodShow({ pageSize, pageNum, sortBy: 'PUB_DATE' })
+  } = await getPodEpisode({ pageSize, pageNum, sortBy: 'PUB_DATE' })
   const totalPages = Math.ceil(+total / +pageSize)
   return (
     <main className="flex flex-col mt-[3px]">
@@ -35,10 +36,62 @@ export default async function Page({
           All podcasts
         </Link>
       </div>
-      <div className={`sticky top-[40px] bg-white dark:bg-black pb-[22px]`}>
+      <div className={`sticky top-[57px] bg-white dark:bg-black pb-[22px] z-[99]`}>
         <Pagination totalPages={totalPages} total={total} title="episodes" />
       </div>
-      <div className={`flex flex-wrap border border-gray-1000 rounded-10px p-[14px] dark:border-fontGry-600`}></div>
+      <div className={`flex flex-wrap border border-gray-1000 rounded-10px p-[14px] dark:border-fontGry-600 overflow-hidden`}>
+        {resultList.map((item: any, ind: number) => {
+          const {
+            coverUrl,
+            episodeTitle,
+            gmtPubDate,
+            showTitle,
+            showCoverUrl,
+            showNotes,
+            episodeId,
+            duration,
+            episodeUrl,
+            enclosureUrl,
+            episodeStatus,
+            showUrl,
+          } = item
+          const cardItem = {
+            coverUrl,
+            episodeTitle,
+            gmtPubDate,
+            showTitle,
+            showCoverUrl,
+            showNotes,
+            episodeId,
+            duration,
+            episodeUrl,
+            enclosureUrl,
+            episodeStatus,
+            showUrl,
+          }
+          const noMb = ind >= resultList.length - 1
+          return (
+            <SearchEpisodesCard
+              item={{
+                coverUrl,
+                episodeTitle,
+                gmtPubDate,
+                showTitle,
+                showCoverUrl,
+                showNotes,
+                episodeId,
+                duration,
+                episodeUrl,
+                enclosureUrl,
+                episodeStatus,
+                showUrl,
+              }}
+              noMb={noMb}
+              key={episodeId}
+            />
+          )
+        })}
+      </div>
     </main>
   )
 }
